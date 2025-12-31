c2a6d805760b05b23b7525506d6b621c728105af
OmerTheProgrammer <omer17508@gmail.com>
2025-12-31 21:48:46 +0200

    fixed all DB's and added Server API side basic 4 functuins to 1 entity

---
+++ Server/Controllers/InsertController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model;
+using System.ComponentModel;
+using System.Text.RegularExpressions;
+using ViewModel;
+
+namespace Server_Manager___API.Controllers
+{
+    // The route template is "api/Insert/[ActionName]"
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class InsertController : Controller
+    {
+        [HttpPost]
+        [ActionName("CategoriesInsertor")]
+        public int InsertCategorie([FromBody] Categories categorie)
+        {
+            CategoriesDB categoriesDB = new CategoriesDB();
+            categoriesDB.Insert(categorie);
+            int ChangedRecords = categoriesDB.SaveChanges();
+            // 200 - OK
+            return ChangedRecords;
+        }
+    }
+}
---
+++ Server/Controllers/UpdateController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model;
+using System.Text.RegularExpressions;
+using ViewModel;
+
+namespace Server_Manager___API.Controllers
+{
+    // The route template is "api/Update/[ActionName]"
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class UpdateController : Controller
+    {
+        [HttpPut]
+        [ActionName("CategoriesUpdateor")]
+        public int UpdateCategories([FromBody] Categories categorie)
+        {
+            CategoriesDB categoriesDB = new CategoriesDB();
+            categoriesDB.Update(categorie);
+            int ChangedRecords = categoriesDB.SaveChanges();
+            // 200 - OK
+            return ChangedRecords;
+        }
+    }
+}
