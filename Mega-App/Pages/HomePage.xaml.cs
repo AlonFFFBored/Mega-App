f1f625ac2d59ad4ea34a6f4c63d12b81e85b87a5
Student <Student@HP-6JW9CV2>
2026-02-08 14:01:41 +0200

    The Banner is Working

---
+++ Mega-App/Pages/HomePage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace Mega_App.Pages
+{
+    public partial class HomePage : Page
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+
+        public HomePage()
+        {
+            InitializeComponent();
+
+            // Setup Auto-Slide timer
+            timer.Interval = TimeSpan.FromSeconds(5);
+            timer.Tick += (s, e) => Next_Click(null, null);
+            timer.Start();
+        }
+
+        private void Next_Click(object sender, RoutedEventArgs e)
+        {
+            if (ImageSlider.SelectedIndex < ImageSlider.Items.Count - 1)
+                ImageSlider.SelectedIndex++;
+            else
+                ImageSlider.SelectedIndex = 0; // Loop back to start
+
+            ScrollToSelected();
+        }
+
+        private void Prev_Click(object sender, RoutedEventArgs e)
+        {
+            if (ImageSlider.SelectedIndex > 0)
+                ImageSlider.SelectedIndex--;
+            else
+                ImageSlider.SelectedIndex = ImageSlider.Items.Count - 1; // Loop to end
+
+            ScrollToSelected();
+        }
+
+        private void ScrollToSelected()
+        {
+            if (ImageSlider.SelectedItem != null)
+            {
+                ImageSlider.ScrollIntoView(ImageSlider.SelectedItem);
+            }
+        }
+    }
+}
